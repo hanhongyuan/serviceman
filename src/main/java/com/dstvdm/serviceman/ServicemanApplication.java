483891c97214adb681558e008a5a5f803c16091a
paulscott56 <pongid56>
2016-03-06 16:12:40 +0200

    Initial commit

---
+++ src/main/java/com/dstvdm/serviceman/ServicemanApplication.java
@@ -0,0 +1,12 @@
+package com.dstvdm.serviceman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicemanApplication.class, args);
+	}
+}
