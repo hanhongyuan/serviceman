ddb7671cb85f66445052c753d18d1418e83a81d2
paulscott56 <pscott209@gmail.com>
2016-03-07 10:02:48 +0200

    Hystrix microservice added

---
+++ circuitbreaker/src/test/java/com/dstvdm/circuitbreaker/CircuitbreakerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dstvdm.circuitbreaker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CircuitbreakerApplication.class)
+public class CircuitbreakerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
