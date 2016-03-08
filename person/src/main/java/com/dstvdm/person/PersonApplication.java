86c7d0f11b942580ad894451170e17073038903e
paulscott56 <pongid56>
2016-03-08 07:04:42 +0200

    Added Person microservice

--- person/src/main/java/com/dstvdm/person/PersonApplication.java
+++ person/src/main/java/com/dstvdm/person/PersonApplication.java
@@ -2,10 +2,12 @@ package com.dstvdm.person;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan
+@EnableEurekaClient
 public class PersonApplication {
 
     public static void main(String... args) {
