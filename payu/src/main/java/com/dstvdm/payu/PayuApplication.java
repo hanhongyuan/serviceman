78279c3bf8e4fc7bf8a02d93896b43f007fb9b06
paulscott56 <pscott209@gmail.com>
2016-03-10 09:47:53 +0200

    Basic payu server impl

--- payu/src/main/java/com/dstvdm/payu/PayuApplication.java
+++ payu/src/main/java/com/dstvdm/payu/PayuApplication.java
@@ -1,11 +1,15 @@
 package com.dstvdm.payu;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 @EnableDiscoveryClient
-@EnableHystrixDashboard
+@EnableHystrix
 public class PayuApplication {
 
 	public static void main(String[] args) {
