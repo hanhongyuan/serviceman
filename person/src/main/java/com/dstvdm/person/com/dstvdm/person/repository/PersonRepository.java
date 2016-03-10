6cc9eb8fbbdae079eaeb4d0069f01718296c99a8
paulscott56 <pongid56>
2016-03-10 08:10:27 +0200

    matti

---
+++ person/src/main/java/com/dstvdm/person/com/dstvdm/person/repository/PersonRepository.java
@@ -0,0 +1,19 @@
+package com.dstvdm.person.com.dstvdm.person.repository;
+
+/**
+ * Created by pscot on 3/7/2016.
+ */
+
+import com.dstvdm.person.com.dstvdm.person.model.Person;
+import org.springframework.data.orient.object.repository.OrientObjectRepository;
+
+/**
+ * DAO for {@link Person}.
+ */
+public interface PersonRepository extends OrientObjectRepository<Person> {
+
+    Person findByEmail(String email);
+
+    Person findByEmailAndActive(String email, boolean active);
+
+}
